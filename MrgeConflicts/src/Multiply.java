4f8e837182e5af2d9ad0f993f6bc76fb2a77af48
Vasavi Elluru <36195939+vasavielluru@users.noreply.github.com>
2021-02-05 09:16:48 +0530

    changes made again

--- MrgeConflicts/src/Multiply.java
+++ MrgeConflicts/src/Multiply.java
@@ -4,7 +4,7 @@ public class Multiply {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int data1=7,data2=9;
-		int product=a*b;
+		int product=data1*data2;
 		
 System.out.println(product);
 	}
