76f45027c3fc401ed299b93d028da439ab17b687
vasavi elluru <elluruvasavi.19993@gmail.com>
2021-02-05 09:13:29 +0530

    merge conflicts

---
+++ MrgeConflicts/src/Multiply.java
@@ -0,0 +1,12 @@
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=7,b=9;
+		int product=a*b;
+		
+System.out.println(product);
+	}
+
+}
